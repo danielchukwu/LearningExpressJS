5df4c1af3f452994a91d33ff7ef232d51d09934f
daniel chukwu <=>
2022-09-17 15:48:13 +0100

    remove /users from path

--- routes/users.js
+++ routes/users.js
@@ -3,11 +3,11 @@ const express = require('express');
 const router = express.Router();
 
 
-router.get('/users', (req, res) => {
+router.get('/', (req, res) => {
    res.send('USERS HOME')
 })
 
-router.get('/users/new', (req, res) => {
+router.get('/new', (req, res) => {
    res.send('Add new users')
 })
 
