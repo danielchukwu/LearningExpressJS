3eccfe978e69cd503236a0301b0fe18357071cb9
daniel chukwu <=>
2022-09-17 13:03:20 +0100

    complete users router

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,15 @@
 const express = require('express');
 
 const router = express.Router();
+
+
+router.get('/users', (req, res) => {
+   res.send('USERS HOME')
+})
+
+router.get('/users/new', (req, res) => {
+   res.send('Add new users')
+})
+
+
+module.exports = router
