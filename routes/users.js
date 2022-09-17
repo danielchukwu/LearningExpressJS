37a08e8bc0592175f054429f982a27241a0eb915
daniel chukwu <=>
2022-09-17 20:24:54 +0100

    dyamic routes

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const res = require('express/lib/response');
 
 const router = express.Router();
 
@@ -10,6 +11,23 @@ router.get('/', (req, res) => {
 router.get('/new', (req, res) => {
    res.send('Add new users')
 })
+
+// dynamic routes should always be at the bottom of the page
+router
+   .route("/:id")
+   .get((req, res) => {
+      res.send(`GET user with id ${req.params.id}`)
+   })
+   .put((req, res) => {
+      res.send(`GET user with id ${req.params.id}`)
+   })
+   .delete((req, res) => {
+      res.send(`GET user with id ${req.params.id}`)
+   })
+
+// middleware - a piece of code that runs inbetween a sent request and the response to that request
+// e.g router.params is a middleware
+
 
 
 module.exports = router
